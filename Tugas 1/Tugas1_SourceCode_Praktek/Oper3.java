f39a349e8e319dfa38331d18a581e127d3dc5f73
leonpratama <leonpratama19@gmail.com>
2026-04-05 20:20:32 +0800

    Add files via upload

---
+++ Tugas 1/Tugas1_SourceCode_Praktek/Oper3.java
@@ -0,0 +1,19 @@
+/* NIM   : 13020240269
+Nama  : Leon Octa Pratama
+Hari/Tanggal : 5 April 2026
+Waktu : 04:00*/
+public class Oper3 {
+public static void main(String[] args) {
+
+if (true && true){ System.out.println(true && true); }
+
+if (true & true) { System.out.println(true & false); }
+
+if (true){ System.out.println(true); }
+
+if (true || true){ System.out.println(true); }
+
+if (true|false) { System.out.println(true|false); }
+
+}
+}
